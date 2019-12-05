10e79976d84b8ba922efdfd195aea41f4c350969
Unknown <leonklute@live.nl>
2019-12-05 11:04:57 +0100

    Now its an arduino compatible library

--- examples/OneEncoder/OneEncoder.ino
+++ examples/OneEncoder/OneEncoder.ino
@@ -4,15 +4,19 @@
 
 Encoder *Left;
 
+const uint8_t CHAN_A = 16;
+const uint8_t CHAN_B = 17;
+const unsigned int FILTERLENGTH = 1023;
+
 void setup() {
   Serial.begin(115200);
   /* Initialize PCNT event queue and PCNT functions */
-  Left = new Encoder(PCNT_UNIT_0, 16, 17, Encoding::X4, 1023);
+  Left = new Encoder(PCNT_UNIT_0, CHAN_A, CHAN_B, Encoding::X4, FILTERLENGTH);
 }
 
 unsigned long count = 0;
 
-void loop() {       
+void loop() {
   /* Wait for the event information passed from PCNT's interrupt handler.
    * Once received, decode the event type and print it on the serial monitor.
    */
